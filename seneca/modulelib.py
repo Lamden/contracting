963838ba6fc4357cc52b47aafa813f55e1f1cf60
Lamden Calrissian <lamdencalrissian@users.noreply.github.com>
2018-04-06 13:39:00 -0500

    Add additional code for RBAC contract.
    * Better export api
    * Contract bugfix
    * AST nodes in whitelist

--- seneca/modulelib.py
+++ seneca/modulelib.py
@@ -1,7 +1,19 @@
 # TODO: Implement auto_exports() and @export
+_exports_dict = {}
 
 def _make_exports(*args):
     #Todo: impelement for types allowable for export, throw exception on others.
     return {a.__name__: a for a in args}
 
-exports = {'make_exports':_make_exports}
+
+
+def export(f):
+    '''function decorator that adds function to export list. 
+    '''
+    _exports_dict[f.__name__] = f
+    return f
+
+
+def make_exports():
+    return _exports_dict
+
