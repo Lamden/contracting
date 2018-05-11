6f08062edb0ea36eaa706f3345dd3caa6eeb5b6f
Lamden Calrissian <lamdencalrissian@users.noreply.github.com>
2018-05-10 18:40:35 -0700

    WiP

--- seneca/storage/__init__.py
+++
@@ -1,1 +0,0 @@
-from seneca.storage import *
--- seneca/storage/kv.py
+++
@@ -1,7 +0,0 @@
-# Placeholder
-
-# Will create a table with 3 columns for each contract:
-# * key (string, unique)
-# * value (serialized to string regardless of type)
-# * type info
-# always upsert
