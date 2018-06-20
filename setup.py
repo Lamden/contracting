b6c4c91aa70d99bc43cb33a1448956a32f362036
Falcon Wong <wywfalcon@gmail.com>
2018-06-20 16:33:46 -0700

    update setup.py and included it in PyPi

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='seneca',
-      version='0.0.1',
-      description='Python-based smart contract language and interpreter.',
+__version__ = '0.0.1'
+
+setup(
+    name='seneca',
+    version=__version__,
+    description='Python-based smart contract language and interpreter.',
+    packages=find_packages(),
     install_requires=open('requirements.txt').readlines(),
-      url='https://github.com/Lamden/seneca',
-      author='Lamden',
-      author_email='team@lamden.io',
-      license='LGPL',
-      packages=['seneca'],
-      zip_safe=False)
+    url='https://github.com/Lamden/vmnet',
+    author='Lamden',
+    email='team@lamden.io',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+    zip_safe=False
+)
