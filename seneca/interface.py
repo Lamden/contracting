b482b9b623122c3d9c93a31865f0a6ab1738ae17
Stuart Farmer <stuart@lamden.io>
2018-08-03 12:52:35 -0700

    mock for a driver for easier integration and testing on the user side

---
+++ seneca/interface.py
@@ -0,0 +1,16 @@
+from seneca.engine.storage.mysql_executer import Executer
+from seneca.engine.storage.mysql_spits_executer import Executer as Spits
+
+class Driver:
+    def __init__(self, username, password, database, port):
+        self.ex = None
+        self.spex = None
+
+    def submit(self, smart_contract):
+        pass
+
+    def flush(self):
+        pass
+
+    def load_director(self, directory):
+        pass
