a0b94bd2aae53b6d4bc3e0cda0d080b345f4d447
Stuart Farmer <stuart@lamden.io>
2018-08-21 16:06:33 -0700

    f

--- seneca/engine/storage/mysql_executer.py
+++ seneca/engine/storage/mysql_executer.py
@@ -9,10 +9,10 @@ TODOs:
     * Table already exists
     * What else?
   * Make sure we're using cursor correctly
-  * Construct executer from existing connection (mainly for testing)f
-'''
+  * Construct executer from existing connection (mainly for testing)f'''
 import MySQLdb
 from seneca.engine.storage.mysql_intermediate import *
+import warnings
 
 requires_commit = [ DeleteRows,
                     UpdateRows,
