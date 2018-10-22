f0564ead2b06c641a5b25a5e0ebf51471e9b2713
Raghu Chalasani <raghupc@hotmail.com>
2018-10-22 12:25:14 -0700

    wip

--- seneca/engine/interpreter.py
+++ seneca/engine/interpreter.py
@@ -1,12 +1,93 @@
 import redis, ast, marshal, array, copy, inspect, types, uuid
 from seneca.constants.whitelists import allowed_ast_types, allowed_import_paths, safe_builtins
 
+#0. will imports wrap into class methods or these wrappers with a default client (db=0) which redis-client can pass in active_db
+#1. how do we wrap these redis commands in a such a way that they will execute against active_db
+   #also reads if any have to use master_db
+   #also a mode where extra meta data will be added or not?
+#2. Directory structure / code organization
+#3. do we need redis_client or can be merged into seneca_client itself
+
 class SenecaInterpreter:
 
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    def __init__(self, loop=None, name=None, get_log_fn=None, concurrent_mode=True):
+        self.loop = loop or asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        name = name or self.__class__.__name__
+        get_log_fn = get_log_fn or SenecaLogger
+        self.log = get_log_fn(name)
+        self.concurrent_mode = concurrent_mode
+        # use dbs for copies  # todo - explore for namespaces also
+        # change / pass port number
+        port = 6379
+        self.master_db = redis.StrictRedis(host='localhost', port=port, db=0)
+        self.worker_dbs = []
+        self.pending_dbs = []
+        self.active_db = None  # pull the first one from worker_dbs when ready
+        # add couple of worker dbs - can be done in a loop for max_number_workers
+        self.max_number_workers = 2    # 2 sufficient for now
+        # different clients have to be opened up front (as redis-py doesn't support select)
+        for db_num in range(self.max_number_workers):
+            db_client = redis.StrictRedis(host='localhost', port=port, db=db_num+1)
+            self.worker_dbs.append(db_client)
+      
+        # connect to server here # persist  # ? use password - hashing of vk + sb_index ?
+
+
+    # will be called upon the receipt of new-block-notification
+    def flush(self, input/result_hash, update_state=True):
+        """
+        If update_state is True, this will also commit the changes
+        to the database. Otherwise, this method will discard any changes
+        """
+        
+        # need to iterate cur_db items and push them to master_db  - same as commit
+        # make sure f_db is the one 
+        f_db = self.pending_dbs.pop(0)    # actually it has to match result_hash
+        if update_state:
+            self.update_master_db(f_db)
+        f_db.flushdb()
+        self.worker_dbs.append(f_db)
+        # pop the right one (should be first one mostly) from active_dbs
+        # save the state to master_db and purge it completely and return it to worker_dbs
+
+    def _start_next_db(self):
+        if len(self.worker_dbs) == 0:
+            return False
+        self.active_db = self.worker_dbs.pop(0)
+        # initialize it - add couple of special k-vs
+        # input-bag hash ? or result-hash at the end so save can check the right one
+        # sync counter ??
+        # return true / false so higher level can throttle it the way it want
+        # phases: 1 - execute contracts, 2 - assertion check / status
+        return True
+
+
+    def db_read(self, key):
+        pass
+
+    def db_write(self, key, value):
+        pass
+
+    def db_assert(self, key, assert_code):
+        pass
+
+    @classmethod
+    def execute(cls, code, scope={}):
+        scope.update({
+            '__builtins__': safe_builtins,
+            '__protected__': Protected(),
+            'export': Export()
+        })
+        exec(code, scope)
+
+
+    # raghu - can't be class variable without locks as many processes will try to update it simultaneously
+    # perhaps in redis itself?
     imports = {}
 
 
+    # raghu todo make them instance methods (not class methods)
     @classmethod
     def get_code_obj(cls, fullname):
         code_obj = cls.r.hget('contracts', fullname)
@@ -55,6 +136,7 @@ class SenecaInterpreter:
                 cls.assert_import_path(module_name)
                 if cls.imports.get(module_name) == 'protected':
                     raise ImportError('"{}" is protected and cannot be imported'.format(module_name))
+                # raghu - falcon, why do we need this?
                 cls.imports[module_name] = 'imported'
 
             elif isinstance(item, ast.ImportFrom):
@@ -63,6 +145,7 @@ class SenecaInterpreter:
                 imported = '.'.join([item.module, module_name])
                 if cls.imports.get(imported) == 'protected':
                     raise ImportError('"{}" is protected and cannot be imported'.format(imported))
+                # raghu - falcon, why do we need this?
                 cls.imports[imported] = 'imported'
 
             # Add the __protected__ decorator if not export
@@ -84,15 +167,6 @@ class SenecaInterpreter:
             ', '.join(map(str, illegal_ast_nodes)))
 
         return tree
-
-    @classmethod
-    def execute(cls, code, scope={}):
-        scope.update({
-            '__builtins__': safe_builtins,
-            '__protected__': Protected(),
-            'export': Export()
-        })
-        exec(code, scope)
 
 class ScopeParser:
     @property
--- seneca/interface/client/seneca_client.py
+++ seneca/interface/client/seneca_client.py
@@ -15,18 +15,60 @@ class ContractStruct:
     This class acts as a simple data structure for holding all information necessary to execute smart contracts using
     the SenecaClient. ContractStructs should be created inside Cilantro, and passed into the SenecaClient for execution
     """
-    def __init__(self, contract_str: str, sender_id: str, sbb_idx: int, order_idx: int):
-        self.contract_str, self.sender_id, self.sbb_index, self.order_number = contract_str, sender_id, sbb_idx, order_idx
+    def __init__(self, contract_str: str, sender_id: str, order_idx: int):
+        self.contract_str, self.sender_id, self.order_number = contract_str, sender_id, order_idx
+
+
 
 
 class SenecaClient:
 
-    def __init__(self, loop=None, name=None, get_log_fn=None):
+    def __init__(self, loop=None, name=None, get_log_fn=None, sb_idx: int):
         name = name or self.__class__.__name__
         get_log_fn = get_log_fn or SenecaLogger
         self.log = get_log_fn(name)
+        self.sb_idx = sb_idx
+        self.interpreter = <seneca-interpreter>
         self.queue = deque()
 
     def finalize(self):
         self.log.notice("Finalizing transactions...")
         pass
+
+
+    def catchup(self):
+        pass
+
+
+    def flush(self, update_state=True):
+        """
+        Flushes internal queue of transactions. If update_state is True, this will also commit the changes
+        to the database. Otherwise, this method will discard any changes
+        """
+        self.interpreter.flush(update_state=update_state)
+        self.queue.clear()
+
+    def run_contract(self, contract):
+        # raghu todo need to update this to ContractStruct or something
+        assert isinstance(contract, OrderingContainer), \
+            "Seneca Interpreter can only interpret OrderingContainer instances"
+
+        txn = self.interpreter.run_contract(contract)
+        self.quue.append(txn)
+
+    def get_tx_queue(self) -> List[TransactionData]:
+        return list(self.queue)
+
+    @property
+    def queue_size(self):
+        return len(self.queue)
+
+    def start(self):
+        assert self.check_contract_future is None, "Start should not be called twice without a .stop() in between!"
+
+        # Check to see if there are valid contracts to be run
+        self.check_contract_future = asyncio.ensure_future(self.check_contract())
+
+    def stop(self):
+        self.check_contract_future.cancel()
+        self.check_contract_future = None
