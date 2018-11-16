2e5f176c7844276357ff070f9a45a7b7ae2f1dc2
wywfalcon <wywfalcon@gmail.com>
2018-11-15 17:19:46 -0800

    i fink da pw is fky

--- scripts/upload_pip.sh
+++ scripts/upload_pip.sh
@@ -3,5 +3,5 @@ then
   sudo python3 setup.py build_ext -i
   sudo python3 setup.py sdist bdist_wheel
   echo "Uploading using twine via user $PYPI_USERNAME ..."
-  twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*
+  twine upload -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD" dist/*
 fi
