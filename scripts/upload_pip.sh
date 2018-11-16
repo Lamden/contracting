1b57a33588f4cd236f7070664ddda573d4a17d5b
wywfalcon <wywfalcon@gmail.com>
2018-11-15 16:58:27 -0800

    ,...

--- scripts/upload_pip.sh
+++ scripts/upload_pip.sh
@@ -2,5 +2,5 @@ if [ "$CIRCLE_BRANCH" == "dev" ]
 then
   sudo python3 setup.py build_ext -i
   sudo python3 setup.py sdist bdist_wheel
-  twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*
+  sudo twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD dist/*
 fi
