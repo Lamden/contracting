52f3b5e3b52f04b9694964fd03ac772e769fed80
wywfalcon <wywfalcon@gmail.com>
2018-11-20 14:16:44 -0800

    include version

--- scripts/upload_pip.sh
+++ scripts/upload_pip.sh
@@ -3,6 +3,5 @@ then
   sudo -E python3 setup.py build_ext -i
   sudo -E python3 setup.py sdist
   echo "Uploading using twine via user $PYPI_USERNAME ..."
-  # sudo rename 's/linux_x86_64.whl/any.whl/' dist/*
   twine upload -u "$PYPI_USERNAME" -p "$PYPI_PASSWORD" dist/*
 fi
