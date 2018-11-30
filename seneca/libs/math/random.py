60610a6d5ea2e6f66a7f5f77f877c545c55a23bb
Stuart Farmer <stuart@lamden.io>
2018-11-29 18:14:16 -0800

    added a fun comment :3

--- seneca/libs/math/random.py
+++ seneca/libs/math/random.py
@@ -1,6 +1,16 @@
 import random
 from seneca.engine.book_keeper import BookKeeper
 
+'''
+    This module wraps and exposes the Python stdlib random functions
+    that can be made deterministic with a random seed and return fixed
+    point precision where possible. This allows some psuedorandom
+    behavior when it is nice to have, but with the caveat that it's
+    based on environmental constants such as the last block hash and
+    public information such as the sender's address to seed the random
+    state.
+'''
+
 
 class Seeded:
     s = False
@@ -21,21 +31,38 @@ def seed(d):
     Seeded.s = True
 
 
+def require_seed(f):
+    def wrapper():
+        assert Seeded.s, 'Random state not seeded. Call seed().'
+        f()
+    return wrapper
+
+
+@require_seed
 def getrandbits(k):
-    assert Seeded.s, 'Random state not seeded. Call seed().'
     return random.getrandbits(k)
 
 
+@require_seed
 def shuffle(l):
-    assert Seeded.s, 'Random state not seeded. Call seed().'
     random.shuffle(l)
 
 
+@require_seed
 def randrange(k):
-    assert Seeded.s, 'Random state not seeded. Call seed().'
     return random.randrange(k)
 
 
+@require_seed
 def randint(a, b):
-    assert Seeded.s, 'Random state not seeded. Call seed().'
     return random.randint(a, b)
+
+
+@require_seed
+def choice(l):
+    return random.choice(l)
+
+
+@require_seed
+def choices(l, k):
+    return random.choices(l, k=k)
