82b8af688dbfeba66d1364721ec5311eab942b3c
wywfalcon <wywfalcon@gmail.com>
2019-01-09 13:01:54 -0800

    fix

--- tests/experiments/dynamic_imports.py
+++ tests/experiments/dynamic_imports.py
@@ -2,48 +2,45 @@ from seneca.tooling import *
 from seneca.engine.interpreter import Seneca
 Seneca.interface.r.flushdb()
 
-def stu_bucks():
+def tau():
     from seneca.libs.datatypes import hmap
 
-    balances = hmap(prefix='balances',
-                    key_type=str,
-                    value_type=float)
+    balances = hmap('balances', str, int)
+
+    @export
+    def transfer(to, amount):
+        assert balances[rt['sender']] >= amount
+
+        balances[to] += amount
+        balances[rt['sender']] -= amount
+
+    # a better way to deal with 'allowances' which are dumb af
+    # and don't reflect real life business operations
+    custodials = hmap('custodials', str, hmap(None, str, int))
+
+    @export
+    def add_to_custodial(to, amount):
+        assert balances[rt['sender']] >= amount
+
+        custodials[rt['sender']][to] += amount
+        balances[rt['sender']] -= amount
+
+    @export
+    def remove_from_custodial(to, amount):
+        assert custodials[rt['sender']][to] >= amount
+
+        balances[rt['sender']] += amount
+        custodials[rt['sender']][to] -= amount
+
+    @export
+    def spend_custodial(_from, amount, to):
+        assert custodials[_from][rt['sender']] >= amount
+
+        balances[to] += amount
+        custodials[_from][rt['sender']] -= amount
+
     @seed
     def seed():
-        balances['stu'] = 1234.0
+        balances['stu'] = 1000000
 
-    @export
-    def check_balance(address):
-        return balances[address]
-
-
-def davis_dollars():
-    from seneca.libs.datatypes import hmap
-
-    balances = hmap(prefix='balances',
-                    key_type=str,
-                    value_type=float)
-    @seed
-    def seed():
-        balances['davis'] = 4321.0 # Both tokens are the same except for this.
-
-    @export
-    def check_balance(address):
-        return balances[address]
-
-publish_function(stu_bucks, 'stu_bucks', 'stu')
-publish_function(davis_dollars, 'davis_dollars', 'davis')
-
-def dynamic_import():
-    from seneca.libs.importing import import_contract
-
-    @export
-    def get_token_balance(token_name, account):
-        token = import_contract(token_name)
-        return token.check_balance(account)
-
-contract = publish_function(dynamic_import, 'dynamic_import', 'stu')
-stu = contract.get_token_balance(token_name='stu_bucks', account='stu')
-davis = contract.get_token_balance(token_name='davis_dollars', account='davis')
-print(stu)
-print(davis)
+contract = publish_function(tau, 'tau', 'stu')
