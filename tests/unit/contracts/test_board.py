98f474c39f7cfa2c0376fb97f03f841c868567e3
wywfalcon <wywfalcon@gmail.com>
2019-01-10 12:47:45 -0800

    using args only when matching contract and function call

--- tests/unit/contracts/test_board.py
+++ tests/unit/contracts/test_board.py
@@ -1,16 +1,19 @@
 from seneca.tooling import *
 from unittest import TestCase, main
+import seneca, os
+
+path = os.path.dirname(seneca.__path__[0])
 
 class TestBoard(TestCase):
 	def setUp(self):
-		f = open('tau.sen.py')
+		f = open('{}/test_contracts/tau.sen.py'.format(path))
 
 		default_driver.r.flushdb()
 		default_driver.publish_code_str(fullname='tau', author='stu', code_str=f.read())
 
 		f.close()
 
-		f = open('board.sen.py')
+		f = open('{}/test_contracts/board.sen.py'.format(path))
 		default_driver.publish_code_str(fullname='board', author='stu', code_str=f.read())
 		f.close()
 		self.contract = ContractWrapper(contract_name='board', driver=default_driver, default_sender='stu')
