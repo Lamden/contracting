d7bbfa06fa7cc94fc3be248aea5bb9394b9017b5
wywfalcon <wywfalcon@gmail.com>
2019-03-07 10:27:34 -0800

    modify RedisProxy

--- seneca/engine/conflict_resolution.py
+++ seneca/engine/conflict_resolution.py
@@ -470,6 +470,45 @@ class RedisProxy:
         cmd.set_params(working_db=self.working_db, master_db=self.master_db, sbb_idx=self.sbb_idx,
                        contract_idx=self.contract_idx, data=self.data)
         return cmd
+    
+    def hexists(self, *args, **kwargs):
+        return bool(self.hget(*args, **kwargs))
+
+    def hincrby(self, hash_key, key, value):
+        res = self.hget(hash_key, key)
+        self.hset(hash_key, key, int(res)+value)
+
+    def hmget(self, hash_key, keys):
+        res = []
+        for key in keys:
+            r = self.hget(hash_key, key)
+            if r: res.append(r)
+        return res
+
+    def hmset(self, hash_key, objs):
+        for key, obj in objs.items():
+            self.hset(hash_key, key, obj)
+
+    def hlen(self, *args, **kwargs):
+        raise NotImplementedError('Not implemented in concurrent mode yet!')
+
+    def scan(self, *args, **kwargs):
+        raise NotImplementedError('Not implemented in concurrent mode yet!')
+
+    def keys(self, *args, **kwargs):
+        raise NotImplementedError('Not implemented in concurrent mode yet!')
+
+    def exists(self, *args, **kwargs):
+        raise NotImplementedError('Not implemented in concurrent mode yet!')
+
+    def rename(self, *args, **kwargs):
+        raise NotImplementedError('Not implemented in concurrent mode yet!')
+
+    def hdel(self, *args, **kwargs):
+        raise NotImplementedError('Not implemented in concurrent mode yet!')
+
+    def delete(self, *args, **kwargs):
+        raise NotImplementedError('Not implemented in concurrent mode yet!')
 
 # print("CRDataMetaRegistery")
 # for k, v in CRDataBase.registry.items():
--- seneca/engine/interpreter/driver.py
+++ seneca/engine/interpreter/driver.py
@@ -1,9 +1,4 @@
 from walrus.tusks.ledisdb import WalrusLedis
-import warnings
-
-
-class NotImplementedException(Exception):
-    pass
 
 
 class Driver(WalrusLedis):
@@ -12,44 +7,3 @@ class Driver(WalrusLedis):
     conflict resolution currently does not support
     """
 
-
-class ConcurrentDriver(Driver):
-
-    def hexists(self, *args, **kwargs):
-        return bool(self.hget(*args, **kwargs))
-
-    def hincrby(self, hash_key, key, value):
-        res = self.hget(hash_key, key)
-        self.hset(hash_key, key, int(res)+value)
-
-    def hmget(self, hash_key, keys):
-        res = []
-        for key in keys:
-            r = self.hget(hash_key, key)
-            if r: res.append(r)
-        return res
-
-    def hmset(self, hash_key, objs):
-        for key, obj in objs.items():
-            self.hset(hash_key, key, obj)
-
-    def hlen(self, *args, **kwargs):
-        raise NotImplementedException('Not implemented in concurrent mode yet!')
-
-    def scan(self, *args, **kwargs):
-        raise NotImplementedException('Not implemented in concurrent mode yet!')
-
-    def keys(self, *args, **kwargs):
-        raise NotImplementedException('Not implemented in concurrent mode yet!')
-
-    def exists(self, *args, **kwargs):
-        raise NotImplementedException('Not implemented in concurrent mode yet!')
-
-    def rename(self, *args, **kwargs):
-        raise NotImplementedException('Not implemented in concurrent mode yet!')
-
-    def hdel(self, *args, **kwargs):
-        raise NotImplementedException('Not implemented in concurrent mode yet!')
-
-    def delete(self, *args, **kwargs):
-        raise NotImplementedException('Not implemented in concurrent mode yet!')
