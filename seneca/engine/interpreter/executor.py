3e4de5bce457fbeec0f44ff1ab06ee448ba26f87
wywfalcon <wywfalcon@gmail.com>
2019-03-15 10:58:38 -0700

    show which driver we are using

--- seneca/engine/interpreter/executor.py
+++ seneca/engine/interpreter/executor.py
@@ -196,7 +196,7 @@ class Executor:
         Scope.scope = Parser.parser_scope
         stamps_used = 0
 
-        print('[SENECA] Executing function in concurrency mode = {} with {}'.format(self.concurrency, Parser.executor))
+        print('[SENECA] Executing function in concurrency mode = {} with {}'.format(self.concurrency, Parser.executor.driver))
 
         if self.metering and not self.tracer.started:
             error = None
