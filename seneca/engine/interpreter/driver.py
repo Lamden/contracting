293c73e4a8468dd8f20adbf58027909f00fa0717
Stuart Farmer <stuart@lamden.io>
2019-04-04 16:58:58 -0700

    incr and exists only sets now

--- seneca/engine/interpreter/driver.py
+++ seneca/engine/interpreter/driver.py
@@ -75,41 +75,16 @@ class Driver(RedisLikeDriver):
         self.conn.delete(key)
 
     def exists(self, key):
-        return self.conn.exists(key)
+        if self.get(key) is not None:
+            return True
+        return False
 
-    def incr(self, key):
-        return self.conn.incr(key)
+    def incr(self, key, amount=1):
+        k = self.get(key)
 
+        if k is None:
+            k = 0
+        k = int(k) + amount
+        self.set(key, k)
 
-
-# class Driver(Redis):
-#     def __init__(self, *args, **kwargs):
-#         kwargs['port'] = 6379
-#         super().__init__(*args, **kwargs)
-#
-#     def hget(self, hash_key, key):
-#         return self.get(hash_key+':'+key)
-#
-#     def hset(self, hash_key, key, value):
-#         return self.set(hash_key+':'+key, value)
-#
-#     def hexists(self, *args, **kwargs):
-#         return bool(self.hget(*args, **kwargs))
-#
-#     def hincrby(self, hash_key, key, value):
-#         res = self.hget(hash_key, key)
-#         self.hset(hash_key, key, int(res)+value)
-#
-#     def hmget(self, hash_key, keys):
-#         res = []
-#         for key in keys:
-#             r = self.hget(hash_key, key)
-#             if r: res.append(r)
-#         return res
-#
-#     def hmset(self, hash_key, objs):
-#         for key, obj in objs.items():
-#             self.hset(hash_key, key, obj)
-#
-#     def xscan(self, *args, **kwargs):
-#         return self.keys(pattern='*')
+        return k
