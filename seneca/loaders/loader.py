a9d4bd779520e9b6f3f837ef290b506e77e38062
Stuart Farmer <stuart@lamden.io>
2019-04-04 11:19:15 -0700

    basic loader done

--- seneca/loaders/loader.py
+++ seneca/loaders/loader.py
@@ -1,63 +1,48 @@
 import sys
 
-from importlib.abc import Loader, ResourceLoader, PathEntryFinder, MetaPathFinder, Finder
-from importlib.machinery import ModuleSpec
+from importlib.abc import Loader, MetaPathFinder
 from importlib import invalidate_caches
-from importlib.util import spec_from_file_location
 
 from redis import Redis
 
+
 class Database:
-	def __init__(self, host='localhost', port=6379, delimiter=':'):
-		self.conn = Redis(host=host, port=port)
-		self.delimiter = delimiter
+    def __init__(self, host='localhost', port=6379, delimiter=':'):
+        self.conn = Redis(host=host, port=port)
+        self.delimiter = delimiter
 
-	def get_contract(self, name):
-		return self.conn.hget(name, 'code').decode()
+    def get_contract(self, name):
+        return self.conn.hget(name, 'code').decode()
 
-d = Database()
 
 class DatabaseFinder(MetaPathFinder):
-	def find_module(fullname, path, target=None):
-		print('path: {}'.format(path))
-		print(fullname)
-		return DatabaseLoader()
+    def find_module(fullname, path, target=None):
+        return DatabaseLoader()
 
 
 class DatabaseLoader(Loader):
-	def create_module(self, spec):
-		# try to grab all of the subdirectories on a VK and stuff them in the spec
-		#assert len(contracts) > 0
-		#print(spec.__dict__)
-		return None
+    def __init__(self):
+        self.d = Database()
 
-	def find_spec(self, spec):
-		pass
+    def create_module(self, spec):
+        return None
 
-	def exec_module(self, module):
-		# fetch the individual contract
-		code = d.get_contract(module.__name__)
-		exec(code, vars(module))
+    def exec_module(self, module):
+        # fetch the individual contract
+        code = self.d.get_contract(module.__name__)
+        exec(code, vars(module))
 
-	def module_repr(self, module):
-		return '<module {!r} (smart contract)>'.format(module.__name__)
+    def module_repr(self, module):
+        return '<module {!r} (smart contract)>'.format(module.__name__)
 
-	def get_data(self, path):
-		print('p: ' + path)
 
 def uninstall_builtins():
-	sys.meta_path.clear()
-	sys.path_hooks.clear()
-	sys.path.clear()
-	sys.path_importer_cache.clear()
-	invalidate_caches()
+    sys.meta_path.clear()
+    sys.path_hooks.clear()
+    sys.path.clear()
+    sys.path_importer_cache.clear()
+    invalidate_caches()
+
 
 def install_database_loader():
-	#sys.path_hooks.append(DatabaseFinder)
-	sys.meta_path.append(DatabaseFinder)
-
-#uninstall_builtins()
-install_database_loader()
-
-import testing
-print(testing.a)
+    sys.meta_path.append(DatabaseFinder)
