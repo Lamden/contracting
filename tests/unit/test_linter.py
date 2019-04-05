f7c990f3763d98a279999f832ba638029f03e820
Stuart Farmer <stuart@lamden.io>
2019-04-05 16:12:16 -0700

    more unit tests on linter

--- tests/unit/test_linter.py
+++ tests/unit/test_linter.py
@@ -35,3 +35,40 @@ def a():
         t = ast.AsyncFunctionDef()
         with self.assertRaises(CompilationException):
             self.l.ast_types(t)
+
+    def test_not_system_variable(self):
+        v = 'package'
+        self.l.not_system_variable(v)
+
+    def test_system_variable(self):
+        v = '__package__'
+        with self.assertRaises(CompilationException):
+            self.l.not_system_variable(v)
+
+    def test_no_starred_imports(self):
+        m = '*'
+        with self.assertRaises(ImportError):
+            self.l.valid_import_path(None, m)
+
+    '''
+    Is blocking all underscore variables really the solution to preventing access to system variables?
+    '''
+    def test_not_system_variable_ast(self):
+        code = '''
+@seneca_export
+def a():
+    __ruh_roh__ = 'shaggy'
+        '''
+
+        with self.assertRaises(CompilationException):
+            c = ast.parse(code)
+            self.l.visit(c)
+
+    def test_not_system_variable_ast_success(self):
+        code = '''
+@seneca_export
+def a():
+    ruh_roh = 'shaggy'
+        '''
+        c = ast.parse(code)
+        self.l.visit(c)
