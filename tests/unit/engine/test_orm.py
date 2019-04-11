04ab198c4860d187ca71d81a84ab75b835089763
Stuart Farmer <stuart@lamden.io>
2019-04-11 15:16:39 -0700

    foreign variable tests done

--- tests/unit/engine/test_orm.py
+++ tests/unit/engine/test_orm.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from seneca.db.driver import ContractDriver
-from seneca.db.orm import Datum, Variable, Hash
+from seneca.db.orm import Datum, Variable, Hash, ForeignVariable
 
 driver = ContractDriver(db=1)
 
@@ -24,7 +24,7 @@ class TestVariable(TestCase):
     def tearDown(self):
         driver.flush()
 
-    def test_set_get(self):
+    def test_set(self):
         contract = 'stustu'
         name = 'balance'
         delimiter = driver.delimiter
@@ -34,10 +34,32 @@ class TestVariable(TestCase):
         v = Variable(contract, name, driver=driver)
         v.set(1000)
 
-        _v = v.get()
-        raw_v = driver.get(raw_key)
+        self.assertEqual(driver.get(raw_key), 1000)
 
-        self.assertEqual(_v, raw_v)
+    def test_get(self):
+        contract = 'stustu'
+        name = 'balance'
+        delimiter = driver.delimiter
+
+        raw_key = '{}{}{}'.format(contract, delimiter, name)
+
+        driver.set(raw_key, 1234)
+
+        v = Variable(contract, name, driver=driver)
+        _v = v.get()
+
+        self.assertEqual(_v, 1234)
+
+    def test_set_get(self):
+        contract = 'stustu'
+        name = 'balance'
+
+        v = Variable(contract, name, driver=driver)
+        v.set(1000)
+
+        _v = v.get()
+
+        self.assertEqual(_v, 1000)
 
 
 class TestHash(TestCase):
@@ -120,3 +142,40 @@ class TestHash(TestCase):
         driver.set(raw_key, 54321)
 
         self.assertEqual(h['stu'], 54321)
+
+
+class TestForeignVariable(TestCase):
+    def setUp(self):
+        driver.flush()
+
+    def tearDown(self):
+        driver.flush()
+
+    def test_set(self):
+        contract = 'stustu'
+        name = 'balance'
+
+        f_contract = 'colinbucks'
+        f_name = 'balances'
+
+        f = ForeignVariable(contract, name, f_contract, f_name, driver=driver)
+
+        with self.assertRaises(ReferenceError):
+            f.set('poo')
+
+    def test_get(self):
+        # set up the foreign variable
+        contract = 'stustu'
+        name = 'balance'
+
+        f_contract = 'colinbucks'
+        f_name = 'balances'
+
+        f = ForeignVariable(contract, name, f_contract, f_name, driver=driver)
+
+        # set the variable using the foreign names (assuming this is another contract namespace)
+        v = Variable(f_contract, f_name, driver=driver)
+        v.set('howdy')
+
+        self.assertEqual(f.get(), 'howdy')
+
