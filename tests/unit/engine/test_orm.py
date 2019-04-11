51bf3105eaeb52853f27d2b1156a2cd267626dd1
Stuart Farmer <stuart@lamden.io>
2019-04-11 14:36:32 -0700

    hash raw set gets

--- tests/unit/engine/test_orm.py
+++ tests/unit/engine/test_orm.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from seneca.db.driver import ContractDriver
-from seneca.db.orm import Datum, Variable
+from seneca.db.orm import Datum, Variable, Hash
 
 driver = ContractDriver(db=1)
 
@@ -39,3 +39,35 @@ class TestVariable(TestCase):
 
         self.assertEqual(_v, raw_v)
 
+
+class TestHash(TestCase):
+    def setUp(self):
+        driver.flush()
+
+    def tearDown(self):
+        driver.flush()
+
+    def test_set_get_raw(self):
+        contract = 'stustu'
+        name = 'balance'
+        delimiter = driver.delimiter
+
+        raw_key_1 = '{}{}{}'.format(contract, delimiter, name)
+        raw_key_1 += ':stu'
+
+        raw_key_2 = '{}{}{}'.format(contract, delimiter, name)
+        raw_key_2 += ':colin'
+
+        h = Hash(contract, name, driver=driver)
+
+        h.set('stu', 1234)
+
+        _h = h.get('stu')
+
+        self.assertEqual(_h, driver.get(raw_key_1))
+
+        h.set('colin', 5678)
+
+        _h2 = h.get('colin')
+
+        self.assertEqual(_h2, driver.get(raw_key_2))
