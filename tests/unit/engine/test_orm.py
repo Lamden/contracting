b74b70995ab35ad4b0584345e4058cc379842c70
Stuart Farmer <stuart@lamden.io>
2019-04-11 14:28:13 -0700

    datum sanity test

---
+++ tests/unit/engine/test_orm.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from seneca.db.driver import ContractDriver
+from seneca.db.orm import Datum
+
+driver = ContractDriver(db=1)
+
+
+class TestDatum(TestCase):
+    def setUp(self):
+        driver.flush()
+
+    def tearDown(self):
+        driver.flush()
+
+    def test_init(self):
+        d = Datum('stustu', 'test', driver)
+        self.assertEqual(d.key, driver.make_key('stustu', 'test'))
+
