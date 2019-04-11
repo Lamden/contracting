8835af36c2ca357365f6c7cde236422bcecee909
Stuart Farmer <stuart@lamden.io>
2019-04-11 14:31:08 -0700

    test variable done

--- tests/unit/engine/test_orm.py
+++ tests/unit/engine/test_orm.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from seneca.db.driver import ContractDriver
-from seneca.db.orm import Datum
+from seneca.db.orm import Datum, Variable
 
 driver = ContractDriver(db=1)
 
@@ -15,4 +15,27 @@ class TestDatum(TestCase):
     def test_init(self):
         d = Datum('stustu', 'test', driver)
         self.assertEqual(d.key, driver.make_key('stustu', 'test'))
+
+
+class TestVariable(TestCase):
+    def setUp(self):
+        driver.flush()
+
+    def tearDown(self):
+        driver.flush()
+
+    def test_set_get(self):
+        contract = 'stustu'
+        name = 'balance'
+        delimiter = driver.delimiter
+
+        raw_key = '{}{}{}'.format(contract, delimiter, name)
+
+        v = Variable(contract, name, driver=driver)
+        v.set(1000)
+
+        _v = v.get()
+        raw_v = driver.get(raw_key)
+
+        self.assertEqual(_v, raw_v)
 
