a99c2bb60a80fa444666137935713334b0244fed
Colin McGrath <cpmcgrat@gmail.com>
2019-04-12 15:41:29 -0700

    Retooled imports to improve readability in-code

--- seneca/execution/executor.py
+++ seneca/execution/executor.py
@@ -1,27 +1,23 @@
+import types
+import threading
+
+from os.path import join
+from functools import lru_cache
+
+from seneca import utils
+from seneca import config
+
+from seneca.parallelism import book_keeper, conflict_resolution
+
+from seneca.db.driver import ContractDriver
 from seneca.execution.parser import Parser
 from seneca.execution.scope import Scope
 #from seneca.metering.tracer import Tracer
-from seneca.config import CODE_OBJ_MAX_CACHE, READ_ONLY_MODE
-import types
-import threading
-from os.path import join
-from functools import lru_cache
-from seneca.utils import Plugins, Assert
-from seneca.parallelism.book_keeper import BookKeeper
-from seneca.parallelism.conflict_resolution import StateProxy
-from seneca.db.driver import ContractDriver
 
 
 class Executor:
 
     def __init__(self, metering=True, concurrency=True, flushall=False):
-        # Colin - Ensure everything is nuked down to minimal viable set
-        #         before we start doing ANYTHING
-        # uninstall_builtins()
-        #
-        # # Colin - Put the database loader in the sys path
-        # install_database_loader()
-
         # Colin - Load in the database driver from the global config
         #         Set driver_proxy to none to indicate it exists and
         #         may be filled later
@@ -46,7 +42,7 @@ class Executor:
         # Colin TODO: Find out why Tracer is not instantiating properly. Raghu also said he wants to pull this out.
         #cu_cost_fname = join(seneca.__path__[0], 'constants', 'cu_costs.const')
         #self.tracer = Tracer(cu_cost_fname)
-        #Plugins.submit_stamps()
+        #utils.Plugins.submit_stamps()
 
     @property
     # Colin - I don't understand what this property is for, why
@@ -55,24 +51,17 @@ class Executor:
     def driver(self):
         if self.concurrency:
             if not self.driver_proxy:
-                info = BookKeeper.get_cr_info()
-                self.driver_proxy = CRDriver(sbb_idx=info['sbb_idx'], contract_idx=info['contract_idx'],
+                info = book_keeper.BookKeeper.get_cr_info()
+                self.driver_proxy = conflict_resolution.StateProxy(sbb_idx=info['sbb_idx'], contract_idx=info['contract_idx'],
                                                data=info['data'])
             else:
-                info = BookKeeper.get_cr_info()
+                info = book_keeper.BookKeeper.get_cr_info()
                 self.driver_proxy.sbb_idx = info['sbb_idx']
                 self.driver_proxy.contract_idx = info['contract_idx']
                 self.driver_proxy.data = info['data']
             return self.driver_proxy
         else:
             return self.driver_base
-
-    # Colin - This should not be happening here. If we want to use
-    #         the Executor class in multiple locations (multiple
-    #         instantiations) we cannot be setting up official
-    #         contracts every time. This should be moved to system
-    #         bootstrap method
-    # Colin TODO: Move to boostrap.py to ensure we are 1-to-1 with boot not instance of executor
 
     def mock_execute(self):
         from types import ModuleType
