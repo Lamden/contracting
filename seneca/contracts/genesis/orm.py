3b5b4bd91bc95411d7cdff7976d413a817903228
Stuart Farmer <stuart@lamden.io>
2019-04-15 14:48:12 -0700

    mock orm contract

---
+++ seneca/contracts/genesis/orm.py
@@ -0,0 +1,19 @@
+from seneca.db.orm import Variable as V
+from seneca.db.orm import Hash as H
+
+
+def var():
+    pass
+
+
+def hash():
+    pass
+
+
+def fvar():
+    pass
+
+
+def fhash():
+    pass
+
