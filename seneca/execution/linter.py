849b317d16ecfd70780a8fc03380a70c2e55730b
Tejas Dave <tejas@lamden.io>
2019-04-19 01:05:56 +0000

    cleanup static methods

--- seneca/execution/linter.py
+++ seneca/execution/linter.py
@@ -8,34 +8,34 @@ from seneca.execution.module import ContractDriver
 
 
 class Linter(ast.NodeVisitor):
-    violations = []
 
     def __init__(self):
         self.log = get_logger('Seneca.Parser')
+        self._violations = []
         self._functions = []
         self._is_one_export = False
         self._is_success = True
         self._constructor_visited = False
         self.driver = ContractDriver()
 
-    @staticmethod
-    def ast_types(t):
+#    @staticmethod
+    def ast_types(self, t):
         if type(t) not in ALLOWED_AST_TYPES:
             str = "Error : Illegal AST type: {}" .format(type(t).__name__)
-            Linter.violations.append(str)
+            self._violations.append(str)
 
-    @staticmethod
-    def not_system_variable(v):
+#    @staticmethod
+    def not_system_variable(self, v):
         if v.startswith('_'):
             str = "Error : Incorrect use of <_> access denied for var : {}".format(v)
-            Linter.violations.append(str)
+            self._violations.append(str)
 
-    @staticmethod
-    def no_nested_imports(node):
+#    @staticmethod
+    def no_nested_imports(self, node):
         for item in node.body:
             if type(item) in [ast.ImportFrom, ast.Import]:
                 str = "Error : Nested import is illegal"
-                Linter.violations.append(str)
+                self._violations.append(str)
 
     def generic_visit(self, node):
         self.ast_types(node)
@@ -58,12 +58,12 @@ class Linter(ast.NodeVisitor):
 
     def visit_ImportFrom(self, node):
         str = 'ImportFrom ast nodes not yet supported.'
-        Linter.violations.append(str)
+        self._violations.append(str)
 
     def validate_imports(self, import_path, module_name=None, alias=None):
         if self.driver.get_contract(import_path) is None:
             str = 'Contract named "{}" does not exist in state.'.format(import_path)
-            Linter.violations.append(str)
+            self._violations.append(str)
 
     def _visit_any_import(self, node):
         self.generic_visit(node)
@@ -92,10 +92,10 @@ class Linter(ast.NodeVisitor):
             if node.value.func.id in ['Variable', 'Hash']:
                 if len(node.value.keywords) > 0:
                     str = 'Keyword overloading not allowed for ORM assignments.'
-                    Linter.violations.append(str)
+                    self._violations.append(str)
             if len(node.targets) > 1:
                 str = 'Multiple targets to an ORM definition is not allowed.'
-                Linter.violations.append(str)
+                self._violations.append(str)
 
         self.generic_visit(node)
         return node
@@ -124,21 +124,21 @@ class Linter(ast.NodeVisitor):
         if len(node.decorator_list) > 1:
             str = 'Function definition can only contain 1 decorator. Currently contains {}.'\
                 .format(len(node.decorator_list))
-            Linter.violations.append(str)
+            self._violations.append(str)
 
         for d in node.decorator_list:
             # Only allow decorators from the allowed set.
             if d.id not in config.VALID_DECORATORS:
                 str = '{} is an invalid decorator. Must be one of {}'.format(d.id,
                                                                              config.VALID_DECORATORS)
-                Linter.violations.append(str)
+                self._violations.append(str)
             if d.id == config.EXPORT_DECORATOR_STRING:
                 self._is_one_export = True
 
             if d.id == config.INIT_DECORATOR_STRING:
                 if self._constructor_visited:
                     str = 'Multiple constructors not allowed.'
-                    Linter.violations.append(str)
+                    self._violations.append(str)
                 self._constructor_visited = True
 
         self.generic_visit(node)
@@ -175,11 +175,11 @@ class Linter(ast.NodeVisitor):
         self._final_checks()
         return self._is_success
 
-    @staticmethod
-    def dump_violations():
+#    @staticmethod
+    def dump_violations(self):
         import pprint
         pp = pprint.PrettyPrinter(indent = 4)
-        pp.pprint(Linter.violations)
+        pp.pprint(self._violations)
 
 
 
