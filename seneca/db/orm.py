54eb063edf5e74de68aff10ac1363ee89b005f20
Stuart Farmer <stuart@lamden.io>
2019-04-23 16:20:50 -0700

    abstracted as helper func

--- seneca/db/orm.py
+++ seneca/db/orm.py
@@ -32,8 +32,28 @@ class Hash(Datum):
     def get(self, item):
         return self.driver.get('{}{}{}'.format(self.key, self.delimiter, item))
 
+    def _validate_key(self, key):
+        if isinstance(key, tuple):
+            assert len(key) <= config.MAX_HASH_DIMENSIONS, 'Too many dimensions ({}) for hash. Max is {}'.format(
+                len(key), config.MAX_HASH_DIMENSIONS
+            )
+
+            new_key_str = ''
+            for k in key:
+                assert not isinstance(k, slice), 'Slices prohibited in hashes.'
+                new_key_str += '{}{}'.format(k, self.delimiter)
+
+            key = new_key_str[:-len(self.delimiter)]
+
+        assert len(key) <= config.MAX_KEY_SIZE, 'Key is too long ({}). Max is {}.'.format(len(key), config.MAX_KEY_SIZE)
+        return key
+
     def __setitem__(self, key, value):
         # handle multiple hashes differently
+        key = self._validate_key(key)
+        self.set(key, value)
+
+    def __getitem__(self, key):
         if isinstance(key, tuple):
             assert len(key) <= config.MAX_HASH_DIMENSIONS, 'Too many dimensions ({}) for hash. Max is {}'.format(
                 len(key), config.MAX_HASH_DIMENSIONS
@@ -48,10 +68,7 @@ class Hash(Datum):
 
         assert len(key) <= config.MAX_KEY_SIZE, 'Key is too long ({}). Max is {}.'.format(len(key), config.MAX_KEY_SIZE)
 
-        self.set(key, value)
-
-    def __getitem__(self, item):
-        return self.get(item)
+        return self.get(key)
 
 
 class ForeignVariable(Variable):
