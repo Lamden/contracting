be7c3de2dbfcedc568f2daee5d298b77b22faef7
Stuart Farmer <stuart@lamden.io>
2019-04-24 17:47:41 -0700

    atomic swap initiate works

---
+++ tests/integration/test_atomic_swap.py
@@ -0,0 +1,84 @@
+from unittest import TestCase
+from seneca.db.driver import ContractDriver
+from seneca.execution.executor import Executor
+from seneca.stdlib.bridge.time import Datetime
+
+def submission_kwargs_for_file(f):
+    # Get the file name only by splitting off directories
+    split = f.split('/')
+    split = split[-1]
+
+    # Now split off the .s
+    split = split.split('.')
+    contract_name = split[0]
+
+    with open(f) as file:
+        contract_code = file.read()
+
+    return {
+        'name': contract_name,
+        'code': contract_code,
+    }
+
+
+TEST_SUBMISSION_KWARGS = {
+    'sender': 'stu',
+    'contract_name': 'submission',
+    'function_name': 'submit_contract'
+}
+
+
+class TestAtomicSwapContract(TestCase):
+    def setUp(self):
+        self.d = ContractDriver()
+        self.d.flush()
+
+        with open('../../seneca/contracts/submission.s.py') as f:
+            contract = f.read()
+
+        self.d.set_contract(name='submission',
+                            code=contract,
+                            author='sys')
+        self.d.commit()
+
+        self.e = Executor()
+
+        print(self.e.execute(**TEST_SUBMISSION_KWARGS,
+                  kwargs=submission_kwargs_for_file('./test_contracts/erc20_clone.s.py')))
+
+        self.e.execute(**TEST_SUBMISSION_KWARGS,
+                  kwargs=submission_kwargs_for_file('./test_contracts/atomic_swaps.s.py'))
+
+    def tearDown(self):
+        #self.d.flush()
+        pass
+
+    def test_initiate_not_enough_approved(self):
+        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
+        status, res = self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
+            'participant': 'raghu',
+            'expiration': Datetime(2020, 1, 1),
+            'hashlock': '6c839446b4d4fa2582af5011730c680b3ee39929f041b7bee6f376211cc710f7',
+            'amount': 5000000
+        })
+
+        self.assertEqual(status, 1)
+        self.assertTrue(isinstance(res, AssertionError))
+
+    def test_initiate_writes_to_correct_key_and_properly(self):
+        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
+        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
+            'participant': 'raghu',
+            'expiration': Datetime(2020, 1, 1),
+            'hashlock': '6c839446b4d4fa2582af5011730c680b3ee39929f041b7bee6f376211cc710f7',
+            'amount': 5
+        })
+
+        _, atomic_swaps = self.e.execute('stu', 'erc20_clone', 'balance_of', kwargs={'account':'atomic_swaps'})
+        _, stu = self.e.execute('stu', 'erc20_clone', 'balance_of', kwargs={'account': 'stu'})
+        _, stu_as = self.e.execute('stu', 'erc20_clone', 'allowance', kwargs={'owner': 'stu', 'spender': 'atomic_swaps'})
+
+        self.assertEqual(atomic_swaps, 5)
+        self.assertEqual(stu, 999995)
+        self.assertEqual(stu_as, 999995)
+
--- tests/integration/test_contracts/atomic_swaps.s.py
+++ tests/integration/test_contracts/atomic_swaps.s.py
@@ -1,19 +1,21 @@
-import currency
+import erc20_clone
 
 swaps = Hash()
 
-
+@seneca_export
 def initiate(participant: str, expiration: datetime, hashlock: str, amount: float):
 
-    assert currency.allowance(ctx.signer, ctx.this) >= amount, \
-        'You cannot initiate an atomic swap without allowing {}' \
-        'at least {} coins.'.format(ctx.this, amount)
+    allowance = erc20_clone.allowance(ctx.signer, ctx.this)
+
+    assert allowance >= amount, \
+        "You cannot initiate an atomic swap without allowing '{}' " \
+        "at least {} coins. You have only allowed {} coins".format(ctx.this, amount, allowance)
 
     swaps[participant, hashlock] = [expiration, amount]
 
-    currency.transfer_from(ctx.signer, ctx.this, amount)
+    erc20_clone.transfer_from(amount, ctx.this, ctx.signer)
 
-
+@seneca_export
 def redeem(secret: str):
 
     hashlock = sha256(secret)
@@ -21,10 +23,10 @@ def redeem(secret: str):
     expiration, amount = swaps[ctx.signer, hashlock]
 
     if expiration >= now:
-        currency.transfer(ctx.signer, amount)
+        erc20_clone.transfer(ctx.signer, amount)
         swaps[ctx.signer, hashlock] = None # change this to respond to the del keyword?
 
-
+@seneca_export
 def refund(participant, secret):
 
     hashlock = sha256(secret)
@@ -32,5 +34,5 @@ def refund(participant, secret):
     expiration, amount = swaps[participant, hashlock]
 
     if expiration < now:
-        currency.transfer(ctx.signer, amount)
+        erc20_clone.transfer(ctx.signer, amount)
         swaps[ctx.signer, hashlock] = None
