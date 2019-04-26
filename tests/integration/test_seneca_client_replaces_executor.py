c8df3f43a4872aba54d48e74e0ab3a274a641117
Stuart Farmer <stuart@lamden.io>
2019-04-25 17:20:24 -0700

    user friendly seneca interface / client done

--- tests/integration/test_seneca_client_replaces_executor.py
+++ tests/integration/test_seneca_client_replaces_executor.py
@@ -1,33 +1,7 @@
 from unittest import TestCase
-from seneca.db.driver import ContractDriver
 from seneca.execution.executor import Executor
 from seneca.stdlib.bridge.time import Datetime
-from seneca.client import SenecaClient, AbstractContract
-
-
-def submission_kwargs_for_file(f):
-    # Get the file name only by splitting off directories
-    split = f.split('/')
-    split = split[-1]
-
-    # Now split off the .s
-    split = split.split('.')
-    contract_name = split[0]
-
-    with open(f) as file:
-        contract_code = file.read()
-
-    return {
-        'name': contract_name,
-        'code': contract_code,
-    }
-
-
-TEST_SUBMISSION_KWARGS = {
-    'sender': 'stu',
-    'contract_name': 'submission',
-    'function_name': 'submit_contract'
-}
+from seneca.client import SenecaClient
 
 
 class TestSenecaClientReplacesExecutor(TestCase):
@@ -73,6 +47,7 @@ class TestSenecaClientReplacesExecutor(TestCase):
 
     def test_initiate_transfers_coins_correctly(self):
         self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
         self.atomic_swaps.initiate(participant='raghu',
                                    expiration=Datetime(2020, 1, 1),
                                    hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
@@ -87,191 +62,171 @@ class TestSenecaClientReplacesExecutor(TestCase):
         self.assertEqual(stu_as, 999995)
 
     def test_initiate_writes_to_correct_key_and_properly(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         key = 'atomic_swaps.swaps:raghu:eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514'
 
-        expiration, amount = self.d.get(key)
+        expiration, amount = self.c.raw_driver.get(key)
         self.assertEqual(expiration, Datetime(2020, 1, 1))
         self.assertEqual(amount, 5)
 
     def test_redeem_on_wrong_secret_fails(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
 
-        s, r = self.e.execute('raghu', 'atomic_swaps', 'redeem', kwargs={'secret': '00'})
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
-        self.assertEqual(s, 1)
-        self.assertEqual(str(r), 'Incorrect sender or secret passed.')
+        with self.assertRaises(AssertionError):
+            self.atomic_swaps.redeem(signer='raghu', secret='00')
 
     def test_redeem_on_wrong_sender_fails(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
-        s, r = self.e.execute('stu', 'atomic_swaps', 'redeem', kwargs={'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'})
-
-        self.assertEqual(s, 1)
-        self.assertEqual(str(r), 'Incorrect sender or secret passed.')
+        with self.assertRaises(AssertionError):
+            self.atomic_swaps.redeem(secret='842b65a7d48e3a3c3f0e9d37eaced0b2')
 
     def test_past_expiration_fails(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         environment = {'now': Datetime(2021, 1, 1)}
 
-        s, r = self.e.execute('raghu', 'atomic_swaps', 'redeem', kwargs={'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'},
-                              environment=environment)
-
-        self.assertEqual(s, 1)
-        self.assertEqual(str(r), 'Swap has expired.')
+        with self.assertRaises(AssertionError):
+            self.atomic_swaps.redeem(secret='842b65a7d48e3a3c3f0e9d37eaced0b2',
+                                     signer='raghu',
+                                     environment=environment)
 
     def test_successful_redeem_transfers_coins_correctly(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         environment = {'now': Datetime(2019, 1, 1)}
 
-        self.e.execute('raghu', 'atomic_swaps', 'redeem', kwargs={'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'},
-                       environment=environment)
+        self.atomic_swaps.redeem(secret='842b65a7d48e3a3c3f0e9d37eaced0b2',
+                                 signer='raghu',
+                                 environment=environment)
 
-        _, atomic_swaps = self.e.execute('stu', 'erc20_clone', 'balance_of', kwargs={'account': 'atomic_swaps'})
-        _, raghu = self.e.execute('stu', 'erc20_clone', 'balance_of', kwargs={'account': 'raghu'})
+        atomic_swaps = self.erc20_clone.balance_of(account='atomic_swaps')
+        raghu = self.erc20_clone.balance_of(account='raghu')
 
         self.assertEqual(raghu, 5)
         self.assertEqual(atomic_swaps, 0)
 
     def test_successful_redeem_deletes_entry(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         environment = {'now': Datetime(2019, 1, 1)}
 
-        self.e.execute('raghu', 'atomic_swaps', 'redeem', kwargs={'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'},
-                       environment=environment)
+        self.atomic_swaps.redeem(secret='842b65a7d48e3a3c3f0e9d37eaced0b2',
+                                 signer='raghu',
+                                 environment=environment)
 
         key = 'atomic_swaps.swaps:raghu:eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514'
-        v = self.d.get(key)
+        v = self.c.raw_driver.get(key)
 
         self.assertEqual(v, None)
 
     def test_refund_works(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         environment = {'now': Datetime(2021, 1, 1)}
 
-        self.e.execute('stu', 'atomic_swaps', 'refund', kwargs={'participant': 'raghu', 'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'},
-                       environment=environment)
+        self.atomic_swaps.refund(participant='raghu', secret='842b65a7d48e3a3c3f0e9d37eaced0b2',
+                                 environment=environment)
 
-        _, atomic_swaps = self.e.execute('stu', 'erc20_clone', 'balance_of', kwargs={'account': 'atomic_swaps'})
-        _, stu = self.e.execute('stu', 'erc20_clone', 'balance_of', kwargs={'account': 'stu'})
+        atomic_swaps = self.erc20_clone.balance_of(account='atomic_swaps')
+        stu = self.erc20_clone.balance_of(account='stu')
 
         self.assertEqual(stu, 1000000)
         self.assertEqual(atomic_swaps, 0)
 
     def test_refund_too_early_fails(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         environment = {'now': Datetime(2019, 1, 1)}
 
-        _, res = self.e.execute('stu', 'atomic_swaps', 'refund',
-                       kwargs={'participant': 'raghu', 'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'},
-                       environment=environment)
-
-        self.assertEqual(str(res), 'Swap has not expired.')
+        with self.assertRaises(AssertionError):
+            self.atomic_swaps.refund(participant='raghu', secret='842b65a7d48e3a3c3f0e9d37eaced0b2',
+                                    environment=environment)
 
     def test_refund_participant_is_signer_fails(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         environment = {'now': Datetime(2021, 1, 1)}
 
-        _, res = self.e.execute('raghu', 'atomic_swaps', 'refund',
-                       kwargs={'participant': 'raghu', 'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'},
-                       environment=environment)
-
-        self.assertEqual(str(res), 'Caller and signer cannot issue a refund.')
+        with self.assertRaises(AssertionError):
+            self.atomic_swaps.refund(participant='raghu', secret='842b65a7d48e3a3c3f0e9d37eaced0b2',
+                                     environment=environment,
+                                     signer='raghu')
 
     def test_refund_fails_with_wrong_secret(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
 
-        environment = {'now': Datetime(2019, 1, 1)}
-
-        _, res = self.e.execute('stu', 'atomic_swaps', 'refund',
-                                kwargs={'participant': 'raghu', 'secret': '00'},
-                                environment=environment)
-
-        self.assertEqual(str(res), 'No swap to refund found.')
-
-    def test_refund_resets_swaps(self):
-        self.e.execute('stu', 'erc20_clone', 'approve', kwargs={'amount': 1000000, 'to': 'atomic_swaps'})
-        self.e.execute('stu', 'atomic_swaps', 'initiate', kwargs={
-            'participant': 'raghu',
-            'expiration': Datetime(2020, 1, 1),
-            'hashlock': 'eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
-            'amount': 5
-        })
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
 
         environment = {'now': Datetime(2021, 1, 1)}
 
-        self.e.execute('stu', 'atomic_swaps', 'refund',
-                       kwargs={'participant': 'raghu', 'secret': '842b65a7d48e3a3c3f0e9d37eaced0b2'},
-                       environment=environment)
+        with self.assertRaises(AssertionError):
+            self.atomic_swaps.refund(participant='raghu', secret='00',
+                                     environment=environment,
+                                     )
+
+    def test_refund_resets_swaps(self):
+        self.erc20_clone.approve(amount=1000000, to='atomic_swaps')
+
+        self.atomic_swaps.initiate(participant='raghu',
+                                   expiration=Datetime(2020, 1, 1),
+                                   hashlock='eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514',
+                                   amount=5)
+
+        environment = {'now': Datetime(2021, 1, 1)}
+
+        self.atomic_swaps.refund(participant='raghu', secret='842b65a7d48e3a3c3f0e9d37eaced0b2',
+                                 environment=environment)
 
         key = 'atomic_swaps.swaps:raghu:eaf48a02d3a4bb3aeb0ecb337f6efb026ee0bbc460652510cff929de78935514'
-        v = self.d.get(key)
+        v = self.c.raw_driver.get(key)
 
         self.assertEqual(v, None)
