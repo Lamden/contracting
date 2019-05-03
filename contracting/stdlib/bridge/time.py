e56e1704cdc4b1defd695ced524dae49cc0c7652
Stuart Farmer <stuart@lamden.io>
2019-05-02 17:30:29 -0700

    basic timedelta implementation, untested

--- contracting/stdlib/bridge/time.py
+++ contracting/stdlib/bridge/time.py
@@ -1,4 +1,6 @@
 from datetime import datetime as dt
+from datetime import timedelta as td
+import decimal
 
 # Redefine a controlled datetime object that feels like a regular Python datetime object but is restricted so that we
 # can regulate the user interaction with it to prevent security attack vectors. It may seem redundant, but it guarantees
@@ -37,6 +39,81 @@ class Datetime:
         return self._datetime != other._datetime
 
 
+class Timedelta:
+    def __init__(self, weeks=0,
+                       days=0,
+                       hours=0,
+                       minutes=0,
+                       seconds=0):
+
+        self._timedelta = td(weeks=weeks, days=days, hours=hours, minutes=minutes, seconds=seconds)
+
+    def __lt__(self, other):
+        return self._timedelta < other._timedelta
+
+    def __le__(self, other):
+        return self._timedelta <= other._timedelta
+
+    def __eq__(self, other):
+        return self._timedelta == other._timedelta
+
+    def __ge__(self, other):
+        return self._timedelta >= other._timedelta
+
+    def __gt__(self, other):
+        return self._timedelta > other._timedelta
+
+    def __ne__(self, other):
+        return self._timedelta != other._timedelta
+
+    # Operator implementations inspired by CPython implementations
+    def __add__(self, other):
+        if isinstance(other, Timedelta):
+            return Timedelta(days=self._timedelta.days + other._timedelta.days,
+                             seconds=self._timedelta.seconds + other._timedelta.seconds)
+        return NotImplemented
+
+    def __sub__(self, other):
+        if isinstance(other, Timedelta):
+            return Timedelta(days=self._timedelta.days - other._timedelta.days,
+                             seconds=self._timedelta.seconds - other._timedelta.seconds,)
+        return NotImplemented
+
+    def __mul__(self, other):
+        if isinstance(other, Timedelta):
+            return Timedelta(days=self._timedelta.days * other._timedelta.days,
+                             seconds=self._timedelta.seconds * other._timedelta.seconds)
+        elif isinstance(other, int):
+            return Timedelta(days=self._timedelta.days * other,
+                             seconds=self._timedelta.seconds * other)
+        elif isinstance(other, float) or isinstance(other, decimal.Decimal):
+            raise NotImplementedError('Decimal division on timedeltas not currently supported.')
+        return NotImplemented
+
+    def __div__(self, other):
+        if isinstance(other, Timedelta):
+            return Timedelta(days=self._timedelta.days * other._timedelta.days,
+                             seconds=self._timedelta.seconds * other._timedelta.seconds)
+        elif isinstance(other, int):
+            return Timedelta(days=self._timedelta.days * other,
+                             seconds=self._timedelta.seconds * other)
+        elif isinstance(other, float) or isinstance(other, decimal.Decimal):
+            raise NotImplementedError('Decimal division on timedeltas not currently supported.')
+        return NotImplemented
+
+
+WEEKS = Timedelta(weeks=1)
+DAYS = Timedelta(days=1)
+HOURS = Timedelta(hours=1)
+MINUTES = Timedelta(minutes=1)
+SECONDS = Timedelta(seconds=1)
+
 exports = {
-    'datetime': Datetime
+    'datetime': Datetime,
+    'timedelta': Timedelta,
+    'WEEKS': WEEKS,
+    'DAYS': DAYS,
+    'HOURS': HOURS,
+    'MINUTES': MINUTES,
+    'SECONDS': SECONDS
 }
