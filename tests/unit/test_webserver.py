63cc4e1f3c612f6ecd9be83e4a5babe55c92d8d8
Stuart Farmer <stuart@lamden.io>
2019-05-14 15:14:47 -0700

    getting contract code test done

--- tests/unit/test_webserver.py
+++ tests/unit/test_webserver.py
@@ -4,10 +4,20 @@ from contracting.webserver import app
 
 class TestTimedelta(TestCase):
     def test_ping_api(self):
-       _, response = app.test_client.get('/')
-       self.assertEqual(response.status, 418)
+        _, response = app.test_client.get('/')
+        self.assertEqual(response.status, 418)
 
     def test_get_all_contracts(self):
         _, response = app.test_client.get('/contracts')
         contracts = response.json.get('contracts')
-        self.assertIn('submission', contracts)
+        self.assertListEqual(['submission'], contracts)
+
+    def test_get_submission_code(self):
+        with open('../../contracting/contracts/submission.s.py') as f:
+            contract = f.read()
+
+        _, response = app.test_client.get('/contracts/submission')
+
+        code = response.json.get('code')
+
+        self.assertEqual(contract, code)
