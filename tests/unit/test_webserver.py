376bc5d39e68737b87a76a7914d6c6e2e925d4dd
Stuart Farmer <stuart@lamden.io>
2019-05-14 15:16:53 -0700

    test methods on api works

--- tests/unit/test_webserver.py
+++ tests/unit/test_webserver.py
@@ -21,3 +21,19 @@ class TestTimedelta(TestCase):
         code = response.json.get('code')
 
         self.assertEqual(contract, code)
+
+    def test_get_submission_methods(self):
+        _, response = app.test_client.get('/contracts/submission/methods')
+
+        method = 'submit_contract'
+        kwargs = ['name', 'code']
+
+        methods = response.json.get('methods')
+
+        self.assertEqual(len(methods), 1)
+
+        test_method = methods[0]
+
+        self.assertEqual(method, test_method.get('name'))
+
+        self.assertListEqual(kwargs, test_method.get('arguments'))
