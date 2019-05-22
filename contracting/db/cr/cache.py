24c610feb063b3d1731d70c71f182111f53d86bf
Raghu Chalasani <raghupc@hotmail.com>
2019-05-22 11:59:31 -0700

    another fix

--- contracting/db/cr/cache.py
+++ contracting/db/cr/cache.py
@@ -240,7 +240,7 @@ class CRCache:
     def rerun_transactions(self):
         self.db.revert(idx=self.rerun_idx)
         self.bag.yield_from(idx=self.rerun_idx)
-        self.results.update(self.executor.execute_bag(self.bag))
+        self.results.update(self.executor.execute_bag(self.bag, driver=self.db))
 
     def resolve_conflicts(self):
         self.prepare_reruns()
