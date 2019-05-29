d122d4ea62c4735143ae27c90a360f0182a3f4ae
Stuart Farmer <stuart@lamden.io>
2019-05-29 16:16:44 -0700

    rerun and prod sets

--- contracting/db/cr/cache.py
+++ contracting/db/cr/cache.py
@@ -237,7 +237,7 @@ class CRCache:
     def rerun_transactions(self):
         self.db.revert(idx=self.rerun_idx)
         self.bag.yield_from(idx=self.rerun_idx)
-        self.results.update(self.executor.execute_bag(self.bag, driver=self.db))
+        self.results.update(self.executor.execute_bag(self.bag, environment=self.bag.environment, driver=self.db))
 
     def resolve_conflicts(self):
         self.prepare_reruns()
