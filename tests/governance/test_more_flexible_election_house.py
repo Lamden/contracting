2b7a8cd57dcc40f9345a8af3ad437228b4206164
Stuart Farmer <stuart@lamden.io>
2019-09-16 16:22:33 -0700

    new election house much more slim

--- tests/governance/test_more_flexible_election_house.py
+++ tests/governance/test_more_flexible_election_house.py
@@ -36,6 +36,8 @@ def election_house():
     @export
     def current_value_for_policy(policy_name: str):
         contract = policy_to_contract[policy_name]
+        assert contract is not None, 'Invalid policy.'
+
         p = I.import_module(contract)
 
         return p.current_value()
@@ -50,8 +52,13 @@ def election_house():
 
         p.vote(vk=ctx.caller, obj=value)
 
+
 def test_policy():
     value = Variable()
+    @construct
+    def seed():
+        value.set('1234')
+
     @export
     def current_value():
         return value.get()
@@ -64,14 +71,75 @@ def test_policy():
         print('this shouldnt matter')
 
 
+def bad_interface():
+    @export
+    def current_value():
+        return 0
+
+
 class TestBetterElectionHouse(TestCase):
     def setUp(self):
         self.client = ContractingClient()
         self.client.submit(election_house)
+
         self.election_house = self.client.get_contract(name='election_house')
 
     def tearDown(self):
         self.client.flush()
 
-    def test_init(self):
-        pass
+    def test_register_doesnt_fail(self):
+        self.client.submit(test_policy, owner='election_house')
+        self.election_house.register_policy(policy_name='testing', contract='test_policy')
+
+    def test_register_without_owner_fails(self):
+        self.client.submit(test_policy)
+        with self.assertRaises(AssertionError):
+            self.election_house.register_policy(policy_name='testing', contract='test_policy')
+
+    def test_register_same_contract_twice_fails(self):
+        self.client.submit(test_policy, owner='election_house')
+        self.election_house.register_policy(policy_name='testing', contract='test_policy')
+
+        with self.assertRaises(Exception):
+            self.election_house.register_policy(policy_name='testing', contract='test_policy')
+
+    def test_register_contract_without_entire_interface_fails(self):
+        self.client.submit(test_policy, owner='election_house')
+
+        with self.assertRaises(Exception):
+            self.election_house.register_policy(policy_name='testing', contract='bad_interface')
+
+    def test_register_same_contract_under_another_name_fails(self):
+        self.client.submit(test_policy, owner='election_house')
+        self.election_house.register_policy(policy_name='testing', contract='test_policy')
+
+        with self.assertRaises(Exception):
+            self.election_house.register_policy(policy_name='testing2', contract='test_policy')
+
+    def test_current_value_for_policy_returns_correct_value(self):
+        self.client.submit(test_policy, owner='election_house')
+        self.election_house.register_policy(policy_name='testing', contract='test_policy')
+
+        res = self.election_house.current_value_for_policy(policy_name='testing')
+
+        self.assertEqual(res, '1234')
+
+    def test_current_value_for_non_existant_policy_fails(self):
+        self.client.submit(test_policy, owner='election_house')
+
+        with self.assertRaises(AssertionError):
+            self.election_house.current_value_for_policy(policy_name='testing')
+
+    def test_vote_delegate_calls_policy(self):
+        self.client.submit(test_policy, owner='election_house')
+        self.election_house.register_policy(policy_name='testing', contract='test_policy')
+        self.election_house.vote(policy_name='testing', value='5678')
+
+    def test_full_vote_flow_works(self):
+        self.client.submit(test_policy, owner='election_house')
+        self.election_house.register_policy(policy_name='testing', contract='test_policy')
+        self.election_house.vote(policy_name='testing', value='5678')
+
+        res = self.election_house.current_value_for_policy(policy_name='testing')
+
+        self.assertEqual(res, '5678')
