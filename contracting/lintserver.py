8539dcf65f283c64486cee5a7c3fc21ab0e85573
Jeff Scott <jeff@Johns-MacBook-Pro.local>
2020-05-19 14:35:17 -0400

    server to lint code

---
+++ contracting/lintserver.py
@@ -0,0 +1,63 @@
+from sanic import Sanic
+from sanic.response import json, text
+from contracting.client import ContractingClient
+from multiprocessing import Queue
+import ast
+import ssl
+
+WEB_SERVER_PORT = 5757
+SSL_WEB_SERVER_PORT = 443
+NUM_WORKERS = 2
+
+app = Sanic(__name__)
+
+ssl_enabled = False
+ssl_cert = '~/.ssh/server.csr'
+ssl_key = '~/.ssh/server.key'
+
+# CORS(app, automatic_options=True)
+client = ContractingClient()
+
+
+@app.route("/", methods=["GET",])
+async def submit_transaction(request):
+    return text('indeed')
+
+
+# Expects json object such that:
+'''
+{
+    'name': 'string',
+    'code': 'string'
+}
+'''
+@app.route('/lint', methods=['POST'])
+async def lint_contract(request):
+    code = request.json.get('code')
+
+    if code is None:
+        return json({'error': 'no code provided'}, status=500)
+
+    violations = client.lint(request.json.get('code'))
+    return json({'violations': violations}, status=200)
+
+
+
+def start_webserver(q):
+    app.queue = q
+    if ssl_enabled:
+        context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain(ssl_cert, keyfile=ssl_key)
+        app.run(host='0.0.0.0', port=SSL_WEB_SERVER_PORT, workers=NUM_WORKERS, debug=False, access_log=False, ssl=context)
+    else:
+        app.run(host='0.0.0.0', port=WEB_SERVER_PORT, workers=NUM_WORKERS, debug=False, access_log=False)
+
+
+if __name__ == '__main__':
+    import pyximport; pyximport.install()
+    if not app.config.REQUEST_MAX_SIZE:
+        app.config.update({
+            'REQUEST_MAX_SIZE': 5,
+            'REQUEST_TIMEOUT': 5
+        })
+    start_webserver(Queue())
