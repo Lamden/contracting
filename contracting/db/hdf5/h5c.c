9cb0de1c6e1ccb8d12a0fbfbaf00347cce984ae6
Nikita Tokariev <mykyta.tokariev@gmail.com>
2022-07-20 22:27:16 -0600

    h5 driver: add lock

--- contracting/db/hdf5/h5c.c
+++ contracting/db/hdf5/h5c.c
@@ -1,9 +1,34 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <hdf5.h>
+#include <libgen.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define BUFSIZE 64000
 #define ATT_NAME "value"
+#define LOCK_DIR "/tmp/"
+#define PATHBUF_SIZE 4096
+
+static char lock_path[PATHBUF_SIZE];
+
+void lock_acquire(char *filepath)
+{
+    strcat(lock_path, LOCK_DIR);
+    strcat(lock_path, basename(filepath));
+    while(mkdir(lock_path, S_IRWXU) != 0)
+        ;
+    memset(lock_path, 0, PATHBUF_SIZE);
+}
+
+void lock_release(char *filepath)
+{
+    strcat(lock_path, LOCK_DIR);
+    strcat(lock_path, basename(filepath));
+    rmdir(lock_path);
+    memset(lock_path, 0, PATHBUF_SIZE);
+}
 
 static PyObject *
 set(PyObject *self, PyObject *args)
@@ -13,6 +38,8 @@ set(PyObject *self, PyObject *args)
     char *filepath, *group, *value;
     if(!PyArg_ParseTuple(args, "ssz", &filepath, &group, &value))
         return NULL;
+
+    lock_acquire(filepath);
 
     hid_t fid = H5Fopen(filepath, H5F_ACC_RDWR, H5P_DEFAULT);
     if(fid < 0)
@@ -43,6 +70,8 @@ set(PyObject *self, PyObject *args)
     H5Gclose(gid);
     H5Fclose(fid);
 
+    lock_release(filepath);
+
     Py_RETURN_NONE;
 }
 
@@ -57,14 +86,20 @@ get(PyObject *self, PyObject *args)
     if(!PyArg_ParseTuple(args, "ss", &filepath, &group))
         return NULL;
 
+    lock_acquire(filepath);
+
     hid_t fid = H5Fopen(filepath, H5F_ACC_RDONLY, H5P_DEFAULT);
     if(fid < 0)
+    {
+        lock_release(filepath);
         Py_RETURN_NONE;
+    }
 
     hid_t aid = H5Aopen_by_name(fid, group, ATT_NAME, H5P_DEFAULT, H5P_DEFAULT);
     if(aid < 0)
     {
         H5Fclose(fid);
+        lock_release(filepath);
         Py_RETURN_NONE;
     }
 
@@ -72,11 +107,16 @@ get(PyObject *self, PyObject *args)
     H5Tset_size(atype, BUFSIZE);
     memset(buf, 0, BUFSIZE);
     if(H5Aread(aid, atype, buf) < 0)
+    {
+        lock_release(filepath);
         Py_RETURN_NONE;
+    }
 
     H5Tclose(atype);
     H5Aclose(aid);
     H5Fclose(fid);
+
+    lock_release(filepath);
 
     return PyUnicode_FromString(buf);
 }
@@ -90,12 +130,16 @@ delete(PyObject *self, PyObject *args)
     if(!PyArg_ParseTuple(args, "ss", &filepath, &group))
         return NULL;
 
+    lock_acquire(filepath);
+
     hid_t fid = H5Fopen(filepath, H5F_ACC_RDWR, H5P_DEFAULT);
     hid_t gid = H5Gopen(fid, group, H5P_DEFAULT);
     H5Adelete(gid, ATT_NAME);
 
     H5Gclose(gid);
     H5Fclose(fid);
+
+    lock_release(filepath);
 
     Py_RETURN_NONE;
 }
