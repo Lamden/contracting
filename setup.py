1be1c7adcee5f645e019871cdb137699154f95e3
crosschainer <eodkwokm@gmail.com>
2024-01-29 09:34:18 +0000

    MAY build on mac now

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ import sys, subprocess, pathlib
 
 major = 0
 
-__version__ = '2.0.10'
+__version__ = "2.0.10"
 
 requirements = [
-    'astor==0.8.1',
-    'pymongo==3.12.3',
-    'pycodestyle==2.10.0',
-    'autopep8==1.5.7',
+    "astor==0.8.1",
+    "pymongo==3.12.3",
+    "pycodestyle==2.10.0",
+    "autopep8==1.5.7",
     "stdlib_list==0.8.0",
-    'motor==2.5.1',
-    'iso8601'
+    "motor==2.5.1",
+    "iso8601",
 ]
 
 ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
@@ -46,28 +46,34 @@ class ve_build_ext(build_ext):
             raise BuildFailed()
         except ValueError as err:
             # this can happen on Windows 64 bit, see Python issue 7511
-            if "'path'" in str(err):    # works with both py 2/3
+            if "'path'" in str(err):  # works with both py 2/3
                 raise BuildFailed()
             raise
 
+
 def pkgconfig(package):
-    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    flag_map = {"-I": "include_dirs", "-L": "library_dirs", "-l": "libraries"}
     res = {}
 
     if platform == "linux" or platform == "linux2":
-        output = subprocess.getoutput('pkg-config --cflags --libs {}'.format(package))
+        output = subprocess.getoutput("pkg-config --cflags --libs {}".format(package))
         for token in output.strip().split():
             key = flag_map.get(token[:2])
             if key:
                 res.setdefault(key, []).append(token[2:])
 
     elif platform == "darwin":
-        res['libraries'] = [f'{package}']
-        output = subprocess.getoutput(f'brew --prefix {package}')
-        if 'Error:' in output or not pathlib.Path(output).is_dir():
-            raise ModuleNotFoundError(f'{output}\nInstall "{package}"" package using brew. "brew install {package}"')
-        res['include_dirs'] = [output + '/include/']
-        res['library_dirs'] = [output + '/lib/']
+        output = subprocess.getoutput("pkg-config --version")
+        if "command not found" in output:
+            raise ModuleNotFoundError(
+                'Install "pkg-config" using brew. "brew install pkg-config"'
+            )
+
+        output = subprocess.getoutput("pkg-config --cflags --libs {}".format(package))
+        for token in output.strip().split():
+            key = flag_map.get(token[:2])
+            if key:
+                res.setdefault(key, []).append(token[2:])
 
     elif platform == "win32":
         raise NotImplementedError("Cannot install on Windows")
@@ -77,31 +83,37 @@ def pkgconfig(package):
     return res
 
 
-
 setup(
-    name='contracting',
+    name="contracting",
     version=__version__,
-    description='Python-based smart contract language and interpreter.',
+    description="Python-based smart contract language and interpreter.",
     packages=find_packages(),
     install_requires=requirements,
-    url='https://github.com/Lamden/contracting',
-    author='Lamden',
-    author_email='team@lamden.io',
+    url="https://github.com/Lamden/contracting",
+    author="Lamden",
+    author_email="team@lamden.io",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     zip_safe=True,
     include_package_data=True,
     ext_modules=[
-        Extension('contracting.execution.metering.tracer', sources=['contracting/execution/metering/tracer.c']),
-        Extension('contracting.db.hdf5.h5c', sources=['contracting/db/hdf5/h5c.c'], **pkgconfig('hdf5'))
+        Extension(
+            "contracting.execution.metering.tracer",
+            sources=["contracting/execution/metering/tracer.c"],
+        ),
+        Extension(
+            "contracting.db.hdf5.h5c",
+            sources=["contracting/db/hdf5/h5c.c"],
+            **pkgconfig("hdf5"),
+        ),
     ],
     cmdclass={
-        'build_ext': ve_build_ext,
+        "build_ext": ve_build_ext,
     },
 )
